b2aa6866a0383340f42c80a6ad6ec039185bb0ff
ozanoner <ozan.oner@gmail.com>
2018-06-06 19:29:42 +0300

    light sensor refactored (PeripheralBleConn)

--- src/light-sensor/src/BleConn.h
+++ src/light-sensor/src/BleConn.h
@@ -10,6 +10,10 @@
 #include "HABleServiceDefs.h"
 #include "HAProvision.h"
 
+// comes from common
+#include "PeripheralBleConn.h"
+
+
 #define DEV_PROVISION_ID LS_ID8
 
 static const uint8_t DEVICE_NAME[] = "LS_ID8";
@@ -19,34 +23,30 @@ static const uint16_t SERVICE_UUID_LIST[] = {LIGHT_SERVICE_UUID};
 
 using namespace std;
 
-class BleConn : private mbed::NonCopyable<BleConn> {
+class BleConn : public PeripheralBleConn {
 private:
-    BLE& ble;
-    events::EventQueue& evq;
-    void scheduleBleEvents(BLE::OnEventsToProcessCallbackContext *context);
+    // in PeripheralBleConn
+    // BLE& ble;
+    // events::EventQueue& evq;
 
     // characteristics
     ReadOnlyGattCharacteristic<uint16_t>  lightState;
 
-    // connection & initialization
-    void onTimeout(const Gap::TimeoutSource_t source);
+    // ble init
     void onInitComplete(BLE::InitializationCompleteCallbackContext* event);
-    void onBleInitError(BLE& ble, ble_error_t error);
-
-    void onConnected(const Gap::ConnectionCallbackParams_t* event);
-    void onDisconnected(const Gap::DisconnectionCallbackParams_t* event);
 
     // data communication
-    // void onDataWritten(const GattWriteCallbackParams* params);
-    void onDataRead(const GattReadCallbackParams* params);
-    void onDataSent(unsigned params);
+    void onDataWritten(const GattWriteCallbackParams* params) override;
+    void onDataRead(const GattReadCallbackParams* params) override;
+    void onDataSent(unsigned params) override;
     
 public:
-    BleConn(EventQueue& evq) : evq(evq), ble(BLE::Instance()), 
+    // BleConn(EventQueue& evq) : evq(evq), ble(BLE::Instance()), 
+    BleConn(EventQueue& evq) : PeripheralBleConn(evq), 
         lightState(LIGHT_STATE_CHARACTERISTIC_UUID, nullptr, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
              {    }
 
-    void init();
+    void init() override;
     void updateSensorState(const uint16_t data);
 };
 
@@ -55,58 +55,17 @@ void BleConn::init() {
     if (ble.hasInitialized())
         return;
 
-    ble.onEventsToProcess(
-        makeFunctionPointer(this, &BleConn::scheduleBleEvents));
-    ble.gap().onTimeout(
-        makeFunctionPointer(this, &BleConn::onTimeout));
+    PeripheralBleConn::init();
 
     ble_error_t error = ble.init(this, &BleConn::onInitComplete);
     if (error) {
-        DPRN("[Error] BLE::init.\n");
+        DPRN("[Error] BLE::init.(%d)\n", error);
         return;
     }
     DPRN("[Info] BleConn::init completed\n");
 }
 
-void BleConn::scheduleBleEvents(BLE::OnEventsToProcessCallbackContext *context)
-{
-    evq.call(mbed::callback(&context->ble, &BLE::processEvents));
-}
 
-/** called if timeout is reached during advertising, scanning
-     *  or connection initiation */
-void BleConn::onTimeout(const Gap::TimeoutSource_t source)
-{
-    switch (source)
-    {
-    case Gap::TIMEOUT_SRC_ADVERTISING:
-        DPRN("[Warning] Stopped advertising early due to timeout parameter\n");
-        break;
-    case Gap::TIMEOUT_SRC_SCAN:
-        DPRN("[Warning] Stopped scanning early due to timeout parameter\n");
-        break;
-    case Gap::TIMEOUT_SRC_CONN:
-        DPRN("[Warning] Failed to connect after scanning %d advertisements\n");
-        // evq.call(this, &BleConn::print_performance);
-        // evq.call(this, &BleConn::demo_mode_end);
-        break;
-    default:
-        DPRN("[Error] Unexpected timeout\n");
-        break;
-    }
-}
-
-void BleConn::onBleInitError(BLE &ble, ble_error_t error)
-{
-    /* Initialization error handling should go here */
-}
-
-void BleConn::onDataRead(const GattReadCallbackParams* params) {
-    DPRN("[info] BleConn::onDataRead. (%x)-(%x)\n", params->handle, params->offset);
-}
-void BleConn::onDataSent(unsigned count) {
-    DPRN("[info] BleConn::onDataSent. (%u)\n", count);
-}
 
 
 void BleConn::onInitComplete(BLE::InitializationCompleteCallbackContext *event)
@@ -117,53 +76,47 @@ void BleConn::onInitComplete(BLE::InitializationCompleteCallbackContext *event)
         this->onBleInitError(this->ble, event->error);
         return;
     }
-
     if(ble.getInstanceID() != BLE::DEFAULT_INSTANCE) {
         DPRN("[Error] Not default BLE instance\n");
         return;
     }
 
-    if(ble.gap().setTxPower(4)!=BLE_ERROR_NONE) {
-        DPRN("[error] setTxPower\n");
-    }
-    ble.gap().setAddress(Gap::AddressType_t::PUBLIC, BLE_NW_ADDR);
+    PeripheralBleConn::onInitComplete();
+
     
-    /* all calls are serialised on the user thread through the event queue */
-    ble.gap().onConnection(this, &BleConn::onConnected);
-    ble.gap().onDisconnection(this, &BleConn::onDisconnected);
-    // ble.gattServer().onDataWritten(this, &BleConn::onDataWritten);
-
-
-    GattCharacteristic* charTable[] = {&this->lightState};
-    GattService service(LIGHT_SERVICE_UUID, charTable, sizeof(charTable) / sizeof(GattCharacteristic *));
-    ble.gattServer().addService(service);
-    ble.gattServer().onDataRead(this, &BleConn::onDataRead);
-    ble.gattServer().onDataSent(this, &BleConn::onDataSent);
-
+    ble.gap().setAddress(Gap::AddressType_t::PUBLIC, BLE_NW_ADDR);
     /* setup advertising */
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, 
         (uint8_t *)SERVICE_UUID_LIST, sizeof(SERVICE_UUID_LIST));
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
-    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
-    ble.gap().startAdvertising();
 
+
+    GattCharacteristic* charTable[] = {&this->lightState};
+    GattService service(LIGHT_SERVICE_UUID, charTable, sizeof(charTable) / sizeof(GattCharacteristic *));
+    ble.gattServer().addService(service);
+
+    // ble communication callbacks
+    ble.gattServer().onDataRead(this, &BleConn::onDataRead); // doesn't run??
+    ble.gattServer().onDataSent(this, &BleConn::onDataSent); // for notification
+    ble.gattServer().onDataWritten(this, &BleConn::onDataWritten);
+
+
+    ble.gap().startAdvertising();
     DPRN("[Info] BleConn::onInitComplete completed. starting ad..\n");
 }
 
 
-
-void BleConn::onConnected(const Gap::ConnectionCallbackParams_t *conn)
-{
-    DPRN("[Info] BleConn::onConnected (%x)\n", conn->handle);
-    this->ble.gap().stopAdvertising();
+void BleConn::onDataRead(const GattReadCallbackParams* params) {
+    PeripheralBleConn::onDataRead(params);
 }
 
-void BleConn::onDisconnected(const Gap::DisconnectionCallbackParams_t *event)
-{
-    DPRN("[Warning] BleConn::onDisconnected\n");
-    this->ble.gap().startAdvertising();
+void BleConn::onDataSent(unsigned count) {
+    PeripheralBleConn::onDataSent(count);
+}
+
+void BleConn::onDataWritten(const GattWriteCallbackParams* params) {
+    PeripheralBleConn::onDataWritten(params);
 }
 
 
